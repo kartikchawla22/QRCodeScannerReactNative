0af93c3bf15d2a60358967eb73e8ab914a0c0439
Kartik Chawla <kchawla1995@gmail.com>
2022-12-09 21:03:46 -0500

    Navigation improvements

--- src/Delegates/HomeRouting/index.tsx
+++ src/Delegates/HomeRouting/index.tsx
@@ -4,14 +4,24 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from '../../Screens/Home';
 import ResultScreen from '../../Screens/Result';
 import Scanner from '../../Screens/Scanner';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
 const Stack = createStackNavigator()
 
-const HomeStackNavigationDelegate = () => {
+const HomeStackNavigationDelegate = ({ navigation, route }: any) => {
+    React.useLayoutEffect(() => {
+        const routeName = getFocusedRouteNameFromRoute(route);
+        if (routeName === "Scanner" || routeName === "Result") {
+            navigation.setOptions({ tabBarStyle: { display: 'none' } });
+        } else {
+            navigation.setOptions({ tabBarStyle: { display: 'flex' } });
+        }
+    }, [navigation, route]);
     return (
         <SafeAreaProvider>
             <Stack.Navigator initialRouteName='Home'>
                 <Stack.Screen name='Home' component={HomeScreen} />
-                <Stack.Screen name='Scanner' component={Scanner} />
+                <Stack.Screen name='Scanner' component={Scanner} options={{ title: "Scan Your NFT" }} />
                 <Stack.Screen name='Result' component={ResultScreen} />
             </Stack.Navigator>
         </SafeAreaProvider>
